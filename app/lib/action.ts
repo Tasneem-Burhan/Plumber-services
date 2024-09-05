0929faa7a17ffc846d492cf36df2c9004fd9cb14
Tasneem-Burhan <batterywala.tasneem@gmail.com>
2024-09-05 12:58:22 +0530

    first commit1

--- app/lib/action.ts
+++ app/lib/action.ts
@@ -1,25 +1,25 @@
 'use server';
 
-import { sql } from '@vercel/postgres';
+// import { sql } from '@vercel/postgres';
 import { z } from "zod";
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
-import bcrypt from 'bcrypt';
+// import bcrypt from 'bcrypt';
 
-export default function CreateTable() {
-    try{
-        await sql `
-        CREATE TABLE User (
-            id INT PRIMARY KEY,
-            name VARCHAR(255),
-            email VARCHAR(255) UNIQUE KEY,
-            password VARCHAR(255),
-            role VARCHAR(255)
-          );`
-    } catch(error) {
-        return {
-            message: "Database error : failed to create TAble User"
-        };
-    }
+// export default function CreateTable() {
+//     try{
+//         await sql `
+//         CREATE TABLE User (
+//             id INT PRIMARY KEY,
+//             name VARCHAR(255),
+//             email VARCHAR(255) UNIQUE KEY,
+//             password VARCHAR(255),
+//             role VARCHAR(255)
+//           );`
+//     } catch(error) {
+//         return {
+//             message: "Database error : failed to create TAble User"
+//         };
+//     }
     
-} 
+// } 
